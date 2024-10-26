671c2dd13f44e09d123010e12489b2f11b92cf7c
VISHYOU-GIT <vishwanathsingh933@gmail.com>
2024-10-26 11:51:06 +0530

    4th

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import busRoute from "./routes/bus.route.js";
-import stopRoute from "./routes/stop.route.js";
+// import stopRoute from "./routes/stop.route.js";
 import express from "express";
 // const cors = require("cors");
 
@@ -11,7 +11,7 @@ const port = 3000;
 // app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));                       
 app.use(express.json());
 app.use("/api/buses", busRoute);
-app.use("/api/stops", stopRoute);
+// app.use("/api/stops", stopRoute);
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`);
 });
